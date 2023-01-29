4e88eed399342daacde40ef95613277570a07b99
VaariDesign <84449958+VaariDesign@users.noreply.github.com>
2023-01-29 21:07:07 +0100

    added ellipse represent face

--- passport.py
+++ passport.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-
 print(cv2. __version__)
 # Load the cascade classifier for detecting faces
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
